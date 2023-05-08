a08d2c8620a98a0b634c8e4ae7b451d76d530553
AdarshRathore223 <adarsh.rathore@s.amity.edu>
2023-05-08 11:25:21 +0530

    debugging in process

--- src/components/AText/AText.tsx
+++ src/components/AText/AText.tsx
@@ -6,10 +6,16 @@ export interface ATextprops {
 }
 
 const AText = (props: ATextprops) => {
+  const mystyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial"
+  };
   console.log(props.color);
   return (
     <svg viewBox="0 0 1350 600">
-        <text x="50%" y="50%" fill={props.color} text-anchor="middle" >
+        <text x="50%" y="50%" fill="#0c0c0c" textAnchor="middle" >
             {props.label }
         </text>
     </svg>
